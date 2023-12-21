3d5388e09c99b72d9926585b4ed0302b797405b8
merna <mernahisham1994@hotmail.com>
2023-12-21 03:47:55 +0200

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { DeviceDetectorService } from 'ngx-device-detector';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AttendeeApp';
+  showHeader:boolean = true;
+  deviceInfo:any = null;
+ 
+  constructor(private router: Router, private deviceService: DeviceDetectorService){
+    this.epicFunction();
+
+    router.events.subscribe((e) => {
+      if (e instanceof NavigationEnd) {
+        if(e.url.toLowerCase() =="/login"||e.url.toLowerCase() =="/setdevice") this.showHeader=false
+        else this.showHeader=true
+      }
+     
+    });
+  }
+  epicFunction() {
+    this.deviceInfo = this.deviceService.getDeviceInfo();
+    const isMobile = this.deviceService.isMobile();
+    const isTablet = this.deviceService.isTablet();
+    const isDesktopDevice = this.deviceService.isDesktop();
+    console.log(this.deviceInfo);
+    console.log(isMobile);  // returns if the device is a mobile device (android / iPhone / windows-phone etc)
+    console.log(isTablet);  // returns if the device us a tablet (iPad etc)
+    console.log(isDesktopDevice); // returns if the app is running on a Desktop browser.
+  }
 }
