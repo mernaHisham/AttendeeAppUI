112ff13db9f7fdf43bdf8c765fad53b100ade9cc
merna <mernahisham1994@hotmail.com>
2023-12-29 08:14:20 +0200

    cmt

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Product } from '../model/product.model';
+import { ProductService } from '../service/product.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  products!: Product[];
 
+  constructor(private productService: ProductService) {}
+
+  ngOnInit() {
+      this.productService.getProducts().then((data) => (this.products = data.slice(0, 5)));
+  }
+
+  getSeverity (product: Product) {
+      switch (product.inventoryStatus) {
+          case 'INSTOCK':
+              return 'success';
+
+          case 'LOWSTOCK':
+              return 'warning';
+
+          case 'OUTOFSTOCK':
+              return 'danger';
+
+          default:
+              return null;
+      }
+  };
 }
+
+
