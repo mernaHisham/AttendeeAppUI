2c62539f17ff549699bee91f5126becd0f2d1958
merna <mernahisham1994@hotmail.com>
2024-01-05 07:35:07 +0200

    fix design

--- src/app/clock/clock.component.ts
+++ src/app/clock/clock.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-clock',
@@ -6,42 +6,31 @@ import { Component, ViewChild } from '@angular/core';
   styleUrls: ['./clock.component.css']
 })
 export class ClockComponent {
-    /*  clock */
-     hours:HTMLCollection = document.getElementsByClassName('hours');
-     minutes:HTMLCollection = document.getElementsByClassName('minutes');
-     seconds:HTMLCollection = document.getElementsByClassName('seconds');
+  /*  clock */
+  hours: HTMLCollection = document.getElementsByClassName('hours');
+  minutes: HTMLCollection = document.getElementsByClassName('minutes');
+  seconds: HTMLCollection = document.getElementsByClassName('seconds');
   ngOnInit() {
     this.clock();
   }
-  hRotationValue:number=0;
-  mRotationValue:number=0;
-  sRotationValue:number=0;
-clock = () => {
-  let today = new Date();
-  let h = (today.getHours() % 12) + today.getMinutes() / 59; // 22 % 12 = 10pm
-  let m = today.getMinutes(); // 0 - 59
-  let s = today.getSeconds(); // 0 - 59
-
-  h *= 30; // 12 * 30 = 360deg
-  m *= 6;
-  s *= 6; // 60 * 6 = 360deg
- 
-  this.hourRotation(h);
-  this.minuteRotation(m);
-  this.secondRotation(s);
-
-  
-
-  // call every second
-  setTimeout(this.clock, 500);
-}
-
-hourRotation = (val:number) => this.hRotationValue = val;
-minuteRotation = (val:number) => this.mRotationValue = val;
-secondRotation = (val:number) => this.sRotationValue = val;
-
-
-
-
-
+  hRotationValue: number = 0;
+  mRotationValue: number = 0;
+  sRotationValue: number = 0;
+  clock = () => {
+    let today = new Date(new Date().toLocaleString("en-US", { timeZone: "Europe/Vienna" }));
+    let h = (today.getHours() % 12) + today.getMinutes() / 59; // 22 % 12 = 10pm
+    let m = today.getMinutes(); // 0 - 59
+    let s = today.getSeconds(); // 0 - 59
+    h *= 30; // 12 * 30 = 360deg
+    m *= 6;
+    s *= 6; // 60 * 6 = 360deg
+    this.hourRotation(h);
+    this.minuteRotation(m);
+    this.secondRotation(s);
+    // call every second
+    setTimeout(this.clock, 500);
+  }
+  hourRotation = (val: number) => this.hRotationValue = val;
+  minuteRotation = (val: number) => this.mRotationValue = val;
+  secondRotation = (val: number) => this.sRotationValue = val;
 }
